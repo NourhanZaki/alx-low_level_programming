5bb2c446ce3c9ee16f94727f12e35e23a737844b
NourhanZaki <nourhanzaki103@gmail.com>
2023-08-12 15:19:18 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/* more headers goes there */
+
+/* check the last digit */
+int main(void)
+{
+	char abc[] = "abcdefghijklmnopqrstuvwxyz";
+    int i = 0;
+    for(i = 0; i<26; i++)
+    {
+        putchar(abc[i]);
+    } 
+         putchar("\n");
+	return (0);
+}
