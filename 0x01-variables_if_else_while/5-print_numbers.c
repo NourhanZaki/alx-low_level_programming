cda67f19775a7ef212ccf7f73e724b3f317043c0
NourhanZaki <nourhanzaki103@gmail.com>
2023-08-12 15:40:41 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* check the last digit */
+int main(void)
+{
+    int i = 0;
+    for(i = 0; i<10; i++)
+    {
+        printf("%d", i);
+    }
+    
+        printf("\n");
+	return (0);
+}
